5b46253113756969332591c51d8da40d3b5bb7ff
johnny850807 <johnny850807@gmail.com>
2020-04-17 21:50:57 +0800

    :sparkles: Problem Markdown Description / Latex

--- src/app/problem-description/problem-description.component.ts
+++ src/app/problem-description/problem-description.component.ts
@@ -1,29 +1,73 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {Problem} from '../models';
 import {ProblemService} from '../services/Services';
 import {ActivatedRoute} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import * as MarkdownIt from 'markdown-it';
+import MarkdownItKatex from 'markdown-it-katex';
+import * as hljs from 'highlight.js';
 
 @Component({
   selector: 'app-problem-description',
   templateUrl: './problem-description.component.html',
   styleUrls: ['./problem-description.component.css']
 })
-export class ProblemDescriptionComponent implements OnInit {
+export class ProblemDescriptionComponent implements OnInit, AfterViewInit {
+
+  constructor(private problemService: ProblemService,
+              private route: ActivatedRoute,
+              private renderer: Renderer2) {
+  }
+
   private problem$: Observable<Problem>;
 
   problem: Problem;
 
-  constructor(private problemService: ProblemService,
-              private route: ActivatedRoute) {
-  }
+  @ViewChild('markdownPanel') markdownPanel: ElementRef;
+  @ViewChild('problemDescriptionPanel') problemDescriptionPanel: ElementRef;
+  @ViewChild('compilationScriptPanel') compilationScriptPanel: ElementRef;
 
   ngOnInit(): void {
     this.problem$ = this.route.parent.params.pipe(switchMap(params =>
       this.problemService.getProblem(+params.problemId)
     ));
-    this.problem$.subscribe(p => this.problem = p);
   }
 
+  ngAfterViewInit(): void {
+    this.problem$.subscribe(p => {
+      this.problem = p;
+      this.renderer.removeClass(this.problemDescriptionPanel.nativeElement, 'hidden');
+      this.renderMarkdown();
+    });
+  }
+
+  private renderMarkdown() {
+    const markdownIt = new MarkdownIt(
+      {
+        html: true,
+        linkify: true,
+        typographer: true,
+        highlight(str, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(lang, str).value;
+            } catch (__) {
+            }
+          }
+          return ''; // use external default escaping
+        }
+      }
+    ).use(MarkdownItKatex);
+    markdownIt.renderer.rules.table_open = () => {
+      return '<table class="table">';
+    };
+    this.renderer.setProperty(this.markdownPanel.nativeElement, 'innerHTML',
+      markdownIt.render(this.problem.markdownDescription));
+    this.renderer.setProperty(this.compilationScriptPanel.nativeElement, 'innerHTML',
+      markdownIt.render('## Compilation \n' +
+        '```shell\n' +
+        this.problem.compilation.script + '\n' +
+        '```'));
+  }
 }
--- src/app/services/impl/StubProblemService.ts
+++ src/app/services/impl/StubProblemService.ts
@@ -2,6 +2,7 @@ import {ProblemService} from '../Services';
 import {Observable, Subject} from 'rxjs';
 import {Compilation, Problem, ProblemItem, SubmittedCodeSpec, TestCase} from '../../models';
 
+
 export class StubProblemService extends ProblemService {
   private readonly PSEUDO_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus,.';
   private readonly problems: Problem[];
@@ -14,19 +15,18 @@ export class StubProblemService extends ProblemService {
     new SubmittedCodeSpec('c', 'function1.c'),
     new SubmittedCodeSpec('c', 'function2.c'),
   ];
-  private readonly compilation = new Compilation('gcc -std=c99 -O2 -pthread prefixsum-seq.c secret.c -o prefixsum-seq');
+  private readonly compilation = EXAMPLE_COMPILATION;
 
 
   constructor() {
     super();
     this.problems = [
-      new Problem(1, '8 Queens', '# Can you solve 8 queens problem?',
-        [this.stubTag1, this.stubTag2], this.submittedCodeMetas, 'zippredProvidedCodes',
-        this.compilation),
-      new Problem(2, 'Parallelizing Merge Sort', '# Merge sort exploits divide and conquer leading itself ' +
-        'to be easy to be parallelized.', [this.stubTag2], this.submittedCodeMetas, 'zippredProvidedCodes',
-        this.compilation),
-      new Problem(3, 'Add Three Numbers', '# Write a program to read three integers a, b, and c, then print their sum.',
+      new Problem(1, '8 Queens', EXAMPLE_MARKDOWN_DESCRIPTION,
+        [this.stubTag1, this.stubTag2],
+        this.submittedCodeMetas, 'zippredProvidedCodes', this.compilation),
+      new Problem(2, 'Parallelizing Merge Sort', EXAMPLE_MARKDOWN_DESCRIPTION,
+        [this.stubTag2], this.submittedCodeMetas, 'zippredProvidedCodes', this.compilation),
+      new Problem(3, 'Add Three Numbers', EXAMPLE_MARKDOWN_DESCRIPTION,
         [this.stubTag1, this.stubTag2], this.submittedCodeMetas, 'zippredProvidedCodes',
         this.compilation)
     ];
@@ -98,7 +98,7 @@ export class StubProblemService extends ProblemService {
 
   getTestCases(problemId: number): Observable<TestCase[]> {
     const testCases$ = new Subject<TestCase[]>();
-    setTimeout( () => {
+    setTimeout(() => {
       testCases$.next(this.testCases);
       testCases$.complete();
     }, 700);
@@ -106,3 +106,59 @@ export class StubProblemService extends ProblemService {
   }
 
 }
+
+
+export let EXAMPLE_MARKDOWN_DESCRIPTION = '<!-- Highest Discount Rate -->\n' +
+  '\n' +
+  '## Task Description ##\n' +
+  'There is a sales coming. Every payment $p$ (a **positive integer**) will have a discount $d$ according to the following table.\n' +
+  '\n' +
+  '| | range of $p$ | discount $d$ |\n' +
+  '|---|---|---|\n' +
+  '| 1 | $0 < p < a$ | 0 |\n' +
+  '| 2 | $a \\leq p < b$ | $v, \\text{ if }p\\text{ is odd}$<br><hr>$w, \\text{ if }p\\text{ is even}$ |\n' +
+  '| 3 | $b \\leq p$ | $x, \\text{ if } p \\equiv 0\\ (mod\\ 3)$<br><hr>$y, ' +
+  '\\text{ if }p \\equiv 1\\ (mod\\ 3)$<br><hr>$z, \\text{ if }p \\equiv 2\\ (mod\\ 3)$ |\n' +
+  '\n' +
+  'We define **discount rate** as $d/p$. Write a program to find the $p$ ' +
+  'that has the highest discount rate. If there are multiple payments that give the highest discount rate, output the smallest one.\n' +
+  '\n' +
+  '## Input Format ##\n' +
+  'There will be 7 lines for integers $a, b, v, w, x, y, z$.\n' +
+  '\n' +
+  '* $v, w, x, y, z \\geq 0$\n' +
+  '* $a > 1$\n' +
+  '* $b > a + 2$\n' +
+  '\n' +
+  '## Output Format ##\n' +
+  'The smallest payment $p$ that has the highest $d/p$ discount rate.\n' +
+  '\n' +
+  '## Sample Input ##\n' +
+  '```\n' +
+  '10\n' +
+  '30\n' +
+  '5\n' +
+  '6\n' +
+  '18\n' +
+  '8\n' +
+  '9\n' +
+  '```\n' +
+  '## Sample Output ##\n' +
+  '```\n' +
+  '10\n' +
+  '```\n' +
+  '## Hint ##\n' +
+  'Since we have not yet discuss the floating point numbers, ' +
+  'so we compare two fractional numbers $a/b$ and $c/d$, by ' +
+  'comparing $a \\* d$ and $b \\* c$ instead, when both $b$ ' +
+  'and $d$ are positive integers.';
+
+
+export let EXAMPLE_COMPILATION = new Compilation(
+  'chmod +x nvprof.sh\n' +
+  'nvcc -Xcompiler "-O2 -fopenmp" main.cu -o main\n' +
+  './nvprof.sh ./main\n' +
+  'cat nvvp.log'
+);
+
+
