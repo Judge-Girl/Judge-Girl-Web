282e0bac755d34fcdff048d80a088ede210fefab
johnny850807 <johnny850807@gmail.com>
2020-05-29 15:13:00 +0800

    :lipstick: Logo Jovy Bab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -69,10 +69,10 @@ import {AngularSplitModule} from 'angular-split';
     {provide: StudentService, useClass: HttpStudentService},
     {provide: ProblemService, useClass: HttpProblemService},
     {provide: SubmissionService, useClass: HttpSubmissionService},
-    {provide: 'BASE_URL', useValue: 'http://localhost'},  // 'http://nami.csie.ntu.edu.tw'
-    {provide: 'PORT_STUDENT_SERVICE', useValue: 33001},
-    {provide: 'PORT_PROBLEM_SERVICE', useValue: 33002},
-    {provide: 'PORT_SUBMISSION_SERVICE', useValue: 33003}
+    {provide: 'BASE_URL', useValue: 'http://luffy.csie.ntu.edu.tw'},  // 'http://luffy.csie.ntu.edu.tw'
+    {provide: 'PORT_STUDENT_SERVICE', useValue: 80},
+    {provide: 'PORT_PROBLEM_SERVICE', useValue: 80},
+    {provide: 'PORT_SUBMISSION_SERVICE', useValue: 80}
   ],
   bootstrap: [AppComponent]
 })
