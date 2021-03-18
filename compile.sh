14c86e1e6a55d2913b6a263122b3e38bb2a37e4f
johnny850807 <johnny850807@gmail.com>
2021-03-18 18:18:05 +0800

    :green_heart: CI/CD on drone

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build . -t judge-k8s-web-compile -f Dockerfile-compile
-docker run --rm  -v $(pwd):/usr/src/app  -v $(pwd)/dist:/usr/src/app/dist judge-k8s-web-compile
+DOCKER_BUILDKIT=1 docker build . -t judge-girl-web-compile -f Dockerfile-compile
+docker run --rm  -v $(pwd):/usr/src/app  -v $(pwd)/dist:/usr/src/app/dist judge-girl-web-compile
