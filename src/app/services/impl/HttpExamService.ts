a17c484f21c3a35bc9921c421a175308fc2be0b2
昭妤 Chao-Yu Lee <chaoyu2330@gmail.com>
2021-05-27 00:15:14 +0800

    Dev/71 Update Problem List UI (#76)
    * Update Problem list UI
    
    * Upadte SUbmission
    
    * exam-questions update UI
    
    * CSS improve git add .

--- src/app/services/impl/HttpExamService.ts
+++ src/app/services/impl/HttpExamService.ts
@@ -1,8 +1,8 @@
-import {ExamService} from '../Services';
+import {ExamService, StudentService} from '../Services';
 
-import {merge, Observable, ReplaySubject, Subject} from 'rxjs';
-import {ExamOverview, ExamItem} from '../../models';
-import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ExamItem, ExamOverview} from '../../models';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Inject, Injectable} from '@angular/core';
 import {HttpRequestCache} from './HttpRequestCache';
 
@@ -14,6 +14,7 @@ export class HttpExamService extends ExamService {
   baseUrl: string;
 
   constructor(protected http: HttpClient,
+              private studentService: StudentService,
               @Inject('EXAM_SERVICE_BASE_URL') baseUrl: string) {
     super();
     this.httpRequestCache = new HttpRequestCache(http);
@@ -21,12 +22,27 @@ export class HttpExamService extends ExamService {
   }
 
   getExamProgressOverview(studentId: number, examId: number): Observable<ExamOverview> {
-    return this.http.get<ExamOverview>(`${this.baseUrl}/api/exams/${examId}/students/${studentId}/overview`);
+    return this.http.get<ExamOverview>(`${this.baseUrl}/api/exams/${examId}/students/${studentId}/overview`,
+      this.httpOptions);
   }
 
   getExamsByStudentId(studentId: number, examStatus: ExamStatus = ExamStatus.all,
                       skip: number = 0, size: number = 50): Observable<ExamItem[]> {
-    return this.httpRequestCache.get(`${this.baseUrl}/api/students/${studentId}/exams?status=${examStatus}&&skip=${skip}&&size=${size}`);
+    return this.httpRequestCache.get(`${this.baseUrl}/api/students/${studentId}/exams?status=${examStatus}&&skip=${skip}&&size=${size}`,
+      this.httpHeaders);
+  }
+
+  // TODO: potentially duplicate code, currently no idea how to extract it.
+  private get httpOptions(): { headers: HttpHeaders } {
+    return {
+      headers: this.httpHeaders
+    };
+  }
+
+  private get httpHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.studentService.currentStudent.token}`
+    });
   }
 }
 
