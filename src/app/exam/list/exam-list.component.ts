13b5ac5daef109254d829c7b09420802df343d0e
johnny850807 <johnny850807@gmail.com>
2021-06-22 23:47:10 +0800

    :sparkles: Revise Exam's UI with status labels

--- src/app/exam/list/exam-list.component.ts
+++ src/app/exam/list/exam-list.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import * as moment from 'moment';
 import {ExamService, StudentService} from '../../../services/Services';
-import {ExamItem} from '../../models';
+import {ExamItem, ExamStatus, getExamStatus} from '../../models';
 import {Router} from '@angular/router';
 import {Observable} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
+import {map, switchMap} from 'rxjs/operators';
 
 
 @Component({
@@ -22,14 +22,58 @@ export class ExamListComponent implements OnInit {
 
   ngOnInit(): void {
     this.examItems$ = this.studentService.loginStudent$.pipe(
-      switchMap(student => this.examService.getExamsByStudentId(student.id)));
+      switchMap(student => this.examService.getExamsByStudentId(student.id)),
+      map(exams => this.sortExamsByStatus(exams)));
   }
 
-  routeToExam(examId: number) {
-    this.router.navigateByUrl(`exams/${examId}`);
+  sortExamsByStatus(exams: ExamItem[]): ExamItem[] {
+    const rank = {};
+    rank[ExamStatus.ONGOING] = 2;
+    rank[ExamStatus.UPCOMING] = 1;
+    rank[ExamStatus.CLOSED] = 0;
+
+    return exams.sort((e1, e2) => {
+      const s1 = getExamStatus(e1);
+      const s2 = getExamStatus(e2);
+      return rank[s2] - rank[s1];
+    });
   }
 
-  getTimeString(date: Date) {
+  routeToExam(exam: ExamItem) {
+    if (getExamStatus(exam) !== ExamStatus.UPCOMING) {
+      this.router.navigateByUrl(`exams/${exam.id}`);
+    }
+  }
+
+  getTimeString(date: number) {
     return moment(date).format('YYYY/MM/DD h:mm A');
+  }
+
+  examStyle(exam: ExamItem) {
+    const examStatus = getExamStatus(exam);
+    switch (examStatus) {
+      case ExamStatus.UPCOMING:
+        return 'upcoming';
+      case ExamStatus.CLOSED:
+        return 'closed';
+      case ExamStatus.ONGOING:
+        return 'ongoing';
+    }
+  }
+
+  describeExamStatus(exam: ExamItem) {
+    const examStatus = getExamStatus(exam);
+    switch (examStatus) {
+      case ExamStatus.UPCOMING:
+        return 'Upcoming';
+      case ExamStatus.CLOSED:
+        return 'Closed';
+      case ExamStatus.ONGOING:
+        return 'Ongoing';
+    }
+  }
+
+  isUpcoming(exam: ExamItem) {
+    return getExamStatus(exam) === ExamStatus.UPCOMING;
   }
 }
